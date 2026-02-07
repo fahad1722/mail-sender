663096c6eaffa7872f3af40d6f643a599941cebe
fahad1722 <mohammedfahad1905@gmail.com>
2026-02-07 17:14:21 +0530

    Added logger for ping

--- mail-sender-backend/src/main/java/com/mail/controller/PingController.java
+++ mail-sender-backend/src/main/java/com/mail/controller/PingController.java
@@ -1,5 +1,7 @@
 package com.mail.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PingController.class);
+
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
+        logger.info("🏓 Ping received! Responding with pong...");
         return ResponseEntity.ok("pong");
     }
 }
